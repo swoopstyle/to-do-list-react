8babc7d7dbe8e60a288f2f3a5152e94ad72b35de
Margarita M <m_martescu@yahoo.com>
2022-01-25 21:07:39 +0000

    add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+          <h1> Hello react project</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
